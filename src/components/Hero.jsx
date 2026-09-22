dbdc66357b4b5f5f519e0c9e97c4503742c34420
Tejas Gupta <tejaskittu5678@gmail.com>
2026-09-23 00:03:09 +0530

    BrandPage using React

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <main className="hero">
+    <main className="hero container">
         <div className='hero-content'>
             <h1>YOUR FEET DESERVE THE BEST</h1>
             <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
             <div className="hero-btn">
                 <button>Shop Now</button>
-                <button>Category</button>
+                <button className='secondary-btn'>Category</button>
             </div>
             <div className="shopping">
                 <p>Also Available on</p>
@@ -18,7 +18,9 @@ const HeroSection = () => {
                 </div>
             </div>
         </div>
-        <div className="hero-image"></div>
+        <div className="hero-image">
+            <img src='/images/shoe_image.png' alt='logo'></img>
+        </div>
     </main>
   )
 }
