77a865f6478f08a7649564fd756a5a15ba05eb7d
Tejas Gupta <tejaskittu5678@gmail.com>
2026-09-22 23:23:06 +0530

    NavBar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css";
+import Navigation from './components/navigation';
+import HeroSection from './components/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <main className="hero">
+        <div className='hero-content'>
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+            <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+            <div className="hero-btn">
+                <button>Shop Now</button>
+                <button>Category</button>
+            </div>
+            <div className="shopping">
+                <p>Also Available on</p>
+                <div className="brand-icon">
+                    <img src='/images/amazon.png' alt='logo'></img>
+                    <img src='/images/flipkart.png' alt='logo'></img>
+                </div>
+            </div>
+        </div>
+        <div className="hero-image"></div>
+    </main>
+  )
+}
+
+export default HeroSection;
